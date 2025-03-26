12fe1ff5862fc926dcd0495704fb616b08293d97
hieuvuminh392005 <hieuvuminh392005@gmail.com>
2025-03-27 00:06:17 +0700

    Xóa thư mục bị đẩy nhầm

--- SP25-B2_PMA1011-SP3/src/main/java/com/example/sprint3/Repository/SanPhamRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.sprint3.Repository;
-
-import com.example.sprint3.Entity.SanPham;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SanPhamRepo extends JpaRepository<SanPham,Integer> {
-}
