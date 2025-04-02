27d04a7e8c8d83e78ee9d6ef35b1aae2a4e39a8a
thanhlc12 <thanhlcph51863@gmail.com>
2025-04-02 18:42:53 +0700

    update

--- src/main/java/com/example/sprin4/repository/NhanVienRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.sprin4.repository;
-
-import com.example.sprin4.entity.NhanVien;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NhanVienRepo extends JpaRepository<NhanVien, Integer> {
-}
